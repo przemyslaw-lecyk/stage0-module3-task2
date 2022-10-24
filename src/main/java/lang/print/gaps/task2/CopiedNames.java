d67f87f1050bcf1555ce4673115c4637010fc1b7
Przemysław Lecyk <przemyslaw@lecyk.net>
2022-10-24 22:10:08 +0200

    Convention naming again

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable1 = 0;
+        int variableOne = 0;
         int variable2 = 2000;
         int value = 1000;
         int variable3 = 4000;
         int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int variable = 5000;
+        variable = 7000;
         int valueX = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,8 +6,8 @@ public class NamingConvention {
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEFIVE = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
